e654f9695893a1e489f829603c87a803aa3600ee
wahid hasan <66632204+wahid47hasan@users.noreply.github.com>
2020-07-08 18:26:19 +0600

    Add files via upload

---
+++ Module 4-solution submit/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+  window.byeSpeaker = byeSpeaker;
+})(window);
